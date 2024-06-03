c9cbf0a20f59d1a7a072743c6d34e823bc76b1cc
Steven <ssebexen@gmail.com>
2024-06-03 18:38:54 -0400

    Renovated about page

--- app/components/ProjectListTileInactive.tsx
+++ app/components/ProjectListTileInactive.tsx
@@ -4,12 +4,12 @@ import Project from "~/interfaces/Project";
 
 interface ProjectListTileProps {
   project: Project
-  onClick?: MouseEventHandler<HTMLDivElement>
+  onClick: () => void
 }
 
 export default function ProjectListTileInactive(props: ProjectListTileProps) {
   return (
-    <div className="h-64 bg-slate-400/50 opacity-50 cursor-pointer hover:bg-slate-600/50" onClick={props.onClick}>
+    <div className="h-64 bg-slate-400/50 opacity-50 cursor-pointer hover:bg-slate-600/50" onClick={() => props.onClick()}>
       <p className="text-center">{props.project.name}</p>
       <img className="mx-auto h-1/2" src={props.project.imgUrl} alt={props.project.imgAlt}></img>
       <p>{props.project.description}</p>
--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -1,12 +1,51 @@
 import { Link } from "@remix-run/react";
+import { useState } from "react";
+
+import AboutTab from "~/components/AboutTab";
+
+const contentWindowGameDev = <div>
+  <p>I've been making games since I was 7 years old.</p>
+</div>;
+
+const contentWindowArt = <div>
+  <p>I was actually president of my high school Art Club. They had low standards apparently.</p>
+</div>;
+
+const contentWindowMusic = <div>
+  <p>I am a "classically-trained" musician who makes very un-classical music using a rather classical workflow.</p>
+</div>;
+
+const contentWindowMeIrl = <div>
+  <p>I am a person.<br/>...that's just one of the many lies you can tell yourself.</p>
+</div>
+
+const contentWindows: {[key: string]: any} = {
+  game_dev: contentWindowGameDev,
+  art: contentWindowArt,
+  music: contentWindowMusic,
+  me_irl: contentWindowMeIrl
+};
 
 export default function About() {
+
+  const [currentTab, setCurrentTab] = useState('game_dev');
+
+  function clickHandler(tabId: string) {
+    setCurrentTab(tabId);
+  }
+
   return (
-        <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-            <h1 className="text-3xl">About Me</h1>
-            <p>I am extraordinarily bad at writing about me pages. Isn't that a fun fact?</p>
-            <p>Wanna see my <a href="https://github.com/ssebexen">GitHub</a>?</p>
-            <p>Probably not. I recommend going <Link to="/">home</Link>.</p>
-        </div>
-    );
+    <div className="flex flex-col">
+      <div className="flex flex-row w-screen">
+        <Link className="flex-1 py-2 text-center bg-slate-300 hover:bg-slate-500" to="/">
+          <p>Home</p>
+        </Link>
+        <AboutTab tabId='game_dev' label='Game Dev' currentTab={currentTab} onClick={clickHandler} />
+        <AboutTab tabId='art' label='Art' currentTab={currentTab} onClick={clickHandler} />
+        <AboutTab tabId='music' label='Music' currentTab={currentTab} onClick={clickHandler} />
+        <AboutTab tabId='me_irl' label='Me IRL' currentTab={currentTab} onClick={clickHandler} />
+      </div>
+      {contentWindows[currentTab]}
+    </div>
+  );
 }
