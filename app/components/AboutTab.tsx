5174861242997781fe3739b2c2dc443b9234d98e
Steven <ssebexen@gmail.com>
2024-06-05 17:32:26 -0400

    Added some more styling to About; content to follow.

--- app/components/AboutTab.tsx
+++ app/components/AboutTab.tsx
@@ -1,19 +1,20 @@
 import { MouseEventHandler } from "react";
 
 interface AboutTabProps {
-  tabId: string
+  tabId: number
   label: string
-  currentTab: string
-  onClick: (arg0: string) => void
+  currentTab: number
+  onClick: (arg0: number) => void
 }
 
-const STYLE_ACTIVE = 'flex-1 py-2 text-center bg-slate-400 hover:bg-slate-500';
-const STYLE_INACTIVE = 'flex-1 py-2 text-center bg-slate-200 hover:bg-slate-500';
+const STYLE_ACTIVE = 'select-none rounded-2xl flex-1 py-2 text-center bg-[#C8A0CE] hover:bg-[#D82D6E]';
+const STYLE_INACTIVE = 'select-none flex-1 py-2 text-center bg-[#F9C4C0] hover:bg-[#D82D6E]';
 
 export default function AboutTab(props: AboutTabProps) {
 
   return (
-    <span className={props.tabId == props.currentTab ? STYLE_ACTIVE : STYLE_INACTIVE} onClick={() => props.onClick(props.tabId)}>
+    <span className={props.tabId == props.currentTab ? STYLE_ACTIVE : STYLE_INACTIVE} onClick={() => props.onClick(props.tabId)}
+      style={{ transitionProperty: 'background-color, border-radius', transitionDuration: '150ms' }}>
       <p>{props.label}</p>
     </span>
   );
--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -2,50 +2,32 @@ import { Link } from "@remix-run/react";
 import { useState } from "react";
 
 import AboutTab from "~/components/AboutTab";
-
-const contentWindowGameDev = <div>
-  <p>I've been making games since I was 7 years old.</p>
-</div>;
-
-const contentWindowArt = <div>
-  <p>I was actually president of my high school Art Club. They had low standards apparently.</p>
-</div>;
-
-const contentWindowMusic = <div>
-  <p>I am a "classically-trained" musician who makes very un-classical music using a rather classical workflow.</p>
-</div>;
-
-const contentWindowMeIrl = <div>
-  <p>I am a person.<br/>...that's just one of the many lies you can tell yourself.</p>
-</div>
-
-const contentWindows: {[key: string]: any} = {
-  game_dev: contentWindowGameDev,
-  art: contentWindowArt,
-  music: contentWindowMusic,
-  me_irl: contentWindowMeIrl
-};
+import contentWindows from "~/data/contentWindows.json";
 
 export default function About() {
 
-  const [currentTab, setCurrentTab] = useState('game_dev');
+  const [currentTab, setCurrentTab] = useState<number>(0);
 
-  function clickHandler(tabId: string) {
+  function clickHandler(tabId: number) {
     setCurrentTab(tabId);
   }
 
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col h-screen bg-[#F9C4C0]">
       <div className="flex flex-row w-screen">
-        <Link className="flex-1 py-2 text-center bg-slate-300 hover:bg-slate-500" to="/">
+        <Link className="flex-1 py-2 text-center bg-[#F9C4C0] hover:bg-[#D82D6E] transition-colors" to="/">
           <p>Home</p>
         </Link>
-        <AboutTab tabId='game_dev' label='Game Dev' currentTab={currentTab} onClick={clickHandler} />
-        <AboutTab tabId='art' label='Art' currentTab={currentTab} onClick={clickHandler} />
-        <AboutTab tabId='music' label='Music' currentTab={currentTab} onClick={clickHandler} />
-        <AboutTab tabId='me_irl' label='Me IRL' currentTab={currentTab} onClick={clickHandler} />
+        <AboutTab tabId={0} label='Game Dev' currentTab={currentTab} onClick={clickHandler} />
+        <AboutTab tabId={1} label='Art' currentTab={currentTab} onClick={clickHandler} />
+        <AboutTab tabId={2} label='Music' currentTab={currentTab} onClick={clickHandler} />
+        <AboutTab tabId={3} label='Me IRL' currentTab={currentTab} onClick={clickHandler} />
       </div>
-      {contentWindows[currentTab]}
+      <div className="flex flex-1 bg-gradient-to-t from-[#F498A1] to-[#F9C4C0]">
+        <div className="flex-1 m-[5%] p-3 rounded-xl drop-shadow-md bg-white">
+          {contentWindows[currentTab]}
+        </div>
+      </div>
     </div>
   );
 }
