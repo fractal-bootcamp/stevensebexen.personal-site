bcfbd8d82f63696d2270896ea9dec7423432bd79
Steven <ssebexen@gmail.com>
2024-06-17 14:26:38 -0400

    Added key prop to ArtGallery items

--- app/components/ArtGallery.tsx
+++ app/components/ArtGallery.tsx
@@ -6,7 +6,7 @@ export default function ArtGallery(props: ArtGalleryProps) {
     <>
     <div className = 'grid grid-cols-2 md:grid-cols-4 gap-4 md:gap-8 my-2 overflow-y-auto'>
       {props.imageUrls?.map(url =>
-          <img src={url} className='object-cover rounded-md aspect-square' />
+          <img key={url} src={url} className='object-cover rounded-md aspect-square' />
       )}
     </div>
     </>
