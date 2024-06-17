848420e498858af6c4d54c0fc41a6b90f1ba3d3b
Steven <ssebexen@gmail.com>
2024-06-17 14:29:20 -0400

    .

--- app/data/contentWindows.ts
+++ app/data/contentWindows.ts
@@ -1,7 +1,7 @@
 const contentWindows = [
-  "I've been making games since 2008. Which sounds a lot more impressive than it actually is. What it really means is that I have a whole list of experiments and half-finished projects, prototypes, and the like.",
-  "I was actually president of my high school Art Club. They had low standards apparently.",
-  "I am a \"classically-trained\" musician who makes very un-classical music using a rather classical workflow.",
-  "I am a person\n...that's just one of the many lies you can tell yourself."
+  'I\'ve been making games since 2008. Which sounds a lot more impressive than it actually is. What it really means is that I have a whole list of experiments and half-finished projects, prototypes, and the like.',
+  'I was actually president of my high school Art Club. They had low standards apparently.',
+  'I am a "classically-trained" musician who makes very un-classical music using a rather classical workflow.',
+  'I am a person.\n...that\'s just one of the many lies you can tell yourself.'
 ]
 export default contentWindows;
